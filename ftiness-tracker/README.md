7d5561302c3bef5f6cd5629a52403be799aafd08
dkam26 <gkam1989@gmail.com>
2019-04-18 22:24:34 +0300

    intial setup

--- ftiness-tracker/README.md
+++ ftiness-tracker/README.md
@@ -1,4 +1,4 @@
-# Fitness
+# FtinessTracker
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.7.
 
--- ftiness-tracker/e2e/src/app.e2e-spec.ts
+++ ftiness-tracker/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to fitness!');
+    expect(page.getTitleText()).toEqual('Welcome to ftiness-tracker!');
   });
 
   afterEach(async () => {
