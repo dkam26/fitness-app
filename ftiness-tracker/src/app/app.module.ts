2cb223c8508554ac4dab67eddab3785425d1b443
dkam26 <gkam1989@gmail.com>
2019-04-18 22:52:46 +0300

    training  components

--- ftiness-tracker/src/app/app.module.ts
+++ ftiness-tracker/src/app/app.module.ts
@@ -7,12 +7,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MaterialModule } from './material.module';
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { PastTrainingComponent } from './training/past-training/past-training.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingComponent
   ],
   imports: [
     BrowserModule,
