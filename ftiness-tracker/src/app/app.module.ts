0b3d42c256d7560e969790cafa8f25548e7b91e2
dkam26 <gkam1989@gmail.com>
2019-04-18 22:50:49 +0300

    login component

--- ftiness-tracker/src/app/app.module.ts
+++ ftiness-tracker/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MaterialModule } from './material.module';
 import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
